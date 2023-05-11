d8c6815c5619c46159c7ed055d40c6c9a692a8cf
Gaurav Kumar <129340333+JoyBoy-00@users.noreply.github.com>
2023-05-11 18:42:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.get("/posts/:title", function(req,res){
   })
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT ,3000, function() {
   console.log("Hello World started on port 3000");
 });
